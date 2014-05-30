9265390437ecc4f656bc7d1c55eb5134b638d913
Luis Fernando Antonioli <luisfernandoantonioli@MacBook-Pro-de-Luis.local>
2014-05-30 07:53:28 -0300

    test commit

--- Obras/MapViewAnnotation.m
+++ Obras/MapViewAnnotation.m
@@ -3,7 +3,7 @@
 //  coreData
 //
 //  Created by Ulisses Malta Santos on 20/01/14.
-//  Copyright (c) 2014 Ulisses Malta Santos. All rights reserved.
+//  Copyright (c) 2014 Ulisses Malta Santos e Luis Fernando Antonioli. All rights reserved.
 //
 
 #import "MapViewAnnotation.h"
